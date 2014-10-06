bda97105b8216ca5dbd04ac744e34ad663b21d9e
Jon Borrelli <borrejj@gmail.com>
2014-10-06 15:58:23 -0400

    file cleaning and update with parallel sim code

---
+++ robustnessFUNC.R
@@ -0,0 +1,114 @@
+ran.unif <- function(motmat, pred = 10, prey = -1, random = F){
+  newmat <- apply(motmat, c(1,2), function(x){
+    if(x==1){runif(1, 0, pred)}else if(x==-1){runif(1, prey, 0)} else{0}
+  })
+  if(random){
+    diag(newmat) <- runif(length(diag(newmat)), -1, 0)
+  }else{diag(newmat) <- -1}
+  
+  return(newmat)
+}
+
+maxRE <- function(rmat){
+  lam.max <- eigen(rmat)$values[which.max(Re(eigen(rmat)$values))]
+  return(lam.max)
+}
+
+eig.analysis <- function(n, matrices, params){
+  require(data.table)
+  dims <- dim(matrices[[1]])
+  cols <- length(matrices)
+  rows <- n
+  eigenMATRIX.re <- matrix(nrow = rows, ncol = cols)
+  eigenMATRIX.im <- matrix(nrow = rows, ncol = cols)
+  samps <- list()
+  for(i in 1:n){
+    ranmat <- lapply(matrices, ran.unif, pred = params[,1],
+                     prey = params[,2], random = T)
+    sampvals <- matrix(nrow = length(ranmat), ncol = dims[1]^2)
+    for(j in 1:length(ranmat)){
+      sampvals[j,] <- ranmat[[j]]
+    }
+    eigs <- sapply(ranmat, maxRE)
+    eigenMATRIX.re[i,] <- Re(eigs)
+    eigenMATRIX.im[i,] <- Im(eigs)
+    samps[[i]] <- as.data.frame(sampvals) 
+  }
+  svals <- cbind(web = rep(1:length(matrices), n), n = rep(1:n, each = cols), rbindlist(samps))
+  return(list(samples = svals, ematrix.re = eigenMATRIX.re, ematrix.im = eigenMATRIX.im))
+}
+
+conversion <- function(tm){
+  for(i in 1:nrow(tm)){
+    for(j in 1:ncol(tm)){
+      if(tm[i,j] == 1){tm[j,i] <- -1}
+    }
+  }
+  return(tm)
+}
+
+randomQSS <- function(S = 10, numweb = 200, chain = 9, total = 14, params){
+  require(NetIndices)
+  require(igraph)
+  mywebs <- list()
+  for(j in 1:numweb){
+    
+    check <- 1
+    while(!check == 0){
+      myweb <- matrix(0, nrow = S, ncol = S)
+      for(i in 1:chain){
+        myweb[i,i+1] <- 1
+      }
+      tophalf <- which(myweb[upper.tri(myweb)] == 0)
+      newones <- sample(tophalf, total-chain)
+      myweb[upper.tri(myweb)][newones] <- 1
+      mywebs[[j]] <- myweb
+      
+      indeg <- apply(myweb, 1, sum)
+      outdeg <- apply(myweb, 2, sum)
+      deg <- indeg + outdeg
+      
+      if(sum(deg == 0) >= 1){check <- 1}else{check <- 0}
+      
+    }
+    
+  }
+  
+  mywebs1 <- lapply(mywebs, conversion)
+  myweb.tl <- lapply(mywebs, TrophInd)
+  emat <- eig.analysis(1000, mywebs1, params) 
+  
+  qss <- apply(emat$ematrix.re, 2, function(x){sum(x<0)/1000})
+  maxtl <- sapply(myweb.tl, function(x){max(x$TL)})
+  meantl <- sapply(myweb.tl, function(x){mean(x$TL)})
+  medtl <- sapply(myweb.tl, function(x){median(x$TL)})
+  sdtl <- sapply(myweb.tl, function(x){sd(x$TL)})
+  diam <- sapply(lapply(mywebs, graph.adjacency), diameter)
+  
+  web.dat <- data.frame(qss, diam, maxtl, meantl, medtl, sdtl)
+  iter.dat <- cbind(par = rep(paste(params, collapse = "_"), nrow(emat$samples)), emat$samples, reals = as.vector(emat$ematrix.re), im = as.vector(emat$ematrix.im))
+  
+  return(list(web.dat, iter.dat))
+}
+
+testLENGTH <- function(webiter = 100, maxchain = 9, totalINT = 14, params){
+  web.dat.ls <- list()
+  iter.dat.ls <- list()
+  for(i in 1:maxchain){
+    cat(i, "\n")
+    test <- randomQSS(S = 10, numweb = webiter, chain = i, total = totalINT, params = params)
+    web.dat.ls[[i]] <- cbind(mxch = rep(i, nrow(test[[1]])), test[[1]])
+    iter.dat.ls[[i]] <- cbind(mxch = rep(i, nrow(test[[2]])), test[[2]])
+  }
+  
+  webdata <- rbindlist(web.dat.ls)
+  iterdata <- rbindlist(iter.dat.ls)
+  
+  return(list(webdata, iterdata))
+}
+
+
+## PARAMS --------------------------------
+pars <- data.frame(pred = c(10, 10, 10, 5, 5, 5, 1, 1, 1), prey = c(-1, -5, -10, -1, -5, -10, -1, -5, -10))
+ints <- c(12, 16, 20, 24, 28)
+allpars <- unique(expand.grid(pars[,1], pars[,2], ints))
